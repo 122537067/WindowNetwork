1538b97cc817e942516df17e08917b90a64a0655
what <122537067@qq.com>
2018-10-22 21:06:00 +0800

    POP3+STMP

---
+++ Windows网络设计/9.HTTP协议应用网页获取/02Frm_GHM.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace GHTM
+{
+    public partial class Frm_GHM : Form
+    {
+        public Frm_GHM()
+        {
+            InitializeComponent();
+        }
+
+        //定义消息常数 
+        public const int TRAN_FINISHED = 0x500; 
+
+        public static IntPtr main_wnd_handle;
+        public static int recv_str_len;
+        public static string recv_str;
+
+        public static MemoryStream ms_recv;
+        //动态链接库引入
+        [DllImport("User32.dll", EntryPoint = "SendMessage")]
+        private static extern int SendMessage(
+        IntPtr hWnd, // handle to destination window 
+        int Msg, // message 
+        int wParam, // first message parameter 
+        int lParam // second message parameter 
+        );
+         
+
+        protected override void DefWndProc(ref Message m)
+        {//窗体消息处理重载
+            switch (m.Msg)
+            {
+                case TRAN_FINISHED:
+
+                    //Encoding gb2312 = Encoding.GetEncoding("gb2312");
+                    Encoding utf8Char = Encoding.GetEncoding("utf-8");
+
+
+                    //recv_str = Encoding.UTF8.GetString(ms_recv.GetBuffer(), 0, recv_str_len);
+                    recv_str = utf8Char.GetString(ms_recv.GetBuffer(), 0, recv_str_len);
+                    textBox1.Text = recv_str;
+                    break;
+                default:
+                    base.DefWndProc(ref m);
+                    break;
+            }
+        }
+          
+        static void thread_GET_html()
+        {
+            IPAddress ipadd_dest = Dns.GetHostEntry(url_str).AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipadd_dest, Int32.Parse("80")); 
+            Socket client_sock = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            //client_sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.NoDelay, 1); 
+            try
+            {
+                client_sock.Blocking = true;
+                client_sock.Connect(remoteEP); 
+                try
+                { 
+                    byte[] SendDataBuffer = new byte[1024];//数据发送缓存
+                    byte[] ReadDataBuffer = new byte[1024];//数据接收缓存
+                    //byte[] b_cmd=Encoding.ASCII.GetBytes("GET / HTTP/1.1\r\n\r\n");
+                    byte[] b_cmd = Encoding.ASCII.GetBytes("GET / HTTP/1.1\r\n" +
+"Accept: text/html, application/xhtml+xml, */*\r\n" +
+"Accept-Language: zh-CN\r\n" +
+"User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko\r\n" +
+"Accept-Encoding: gzip, deflate\r\n" +
+"Host: www.cnki.net\r\n" +
+"DNT: 1\r\n" +
+"Connection: Keep-Alive\r\n" +
+"Cookie: SID=91001; LID=WEEvREcwSlJHSldRa1FhdkJkcGkxUDRKTEcrQmxCK2RKRFdlWEpPKzhKTT0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4ggI8Fm4gTkoUKaID8j8gFw!!; Ecp_ClientId=4170925144401603240; c_m_LinID=LinID=WEEvREcwSlJHSldRa1FhdkJkcGkxUDRKTEcrQmxCK2RKRFdlWEpPKzhKTT0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4ggI8Fm4gTkoUKaID8j8gFw!!&ot=09/25/2017 15:22:24; c_m_expire=2017-09-25 15:22:24; Ecp_LoginStuts={\"IsAutoLogin\":false,\"UserName\":\"\",\"ShowName\":\"\",\"UserType\":\"bk\",\"r\":\"tcvFjM\"}\r\n" +
+                   "\r\n");
+                     Array.Copy(b_cmd, SendDataBuffer, b_cmd.Length);
+                    client_sock.Send(SendDataBuffer, SocketFlags.None);
+                    ms_recv.Seek(0, SeekOrigin.Begin);
+                    recv_str_len = 0;
+                    int recv_package_len = 0;
+                    recv_package_len = client_sock.Receive(ReadDataBuffer, 1024, SocketFlags.None);
+                    while (recv_package_len != 0)
+                    {
+                        recv_str_len += recv_package_len;
+                        ms_recv.Write(ReadDataBuffer, 0, recv_package_len);
+                        recv_package_len = client_sock.Receive(ReadDataBuffer, 1024, SocketFlags.None);
+                    }
+                    client_sock.Close();
+                    SendMessage(main_wnd_handle, TRAN_FINISHED, 100, 100);
+                }
+                catch (SocketException se3)
+                {
+                    MessageBox.Show("客户端异常" + se3.Message);
+                }
+            }
+            catch (SocketException se1)
+            {
+                MessageBox.Show("SocketException 客户端连接不到服务器呢" + se1.Message);
+            }
+            catch (Exception se2)
+            {
+                MessageBox.Show("客户端异常" + se2.Message);
+            } 
+        }
+
+        public static string url_str;
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            url_str = textBox2.Text;
+            //启动线程			
+            Thread workThread = new Thread(new ThreadStart(thread_GET_html));
+            workThread.IsBackground = true;
+            workThread.Start();
+
+        }
+
+        private void Frm_GHM_Load(object sender, EventArgs e)
+        {
+            main_wnd_handle = this.Handle;
+            ms_recv = new MemoryStream(5000000);
+        }
+    }
+}
